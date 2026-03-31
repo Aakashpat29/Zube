525e0b9b99f2230a6b08c264f73044aec6f3f76c
Aakash Patel <patelaakash2904@gmail.com>
2026-03-31 18:18:37 +0530

    added root route

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+
+// Add this near the top (after app = express())
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🚀 YouTube Clone Backend is Running!</h1>
+    <p>Go to <a href="/api/v1/healthcheck">/api/v1/healthcheck</a></p>
+  `);
+});
+
 //routes import
 
 import userRouter from "./src/routes/user.routes.js";
@@ -38,5 +47,8 @@ app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 app.use("/api/v1/playlist", playlistRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
+
+// Ignore favicon requests
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 export default app ;
